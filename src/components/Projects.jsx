f6789f1bf6d143234b0bb4d68cd7a31954bf7f0b
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-03-04 23:25:28 -0800

    changed width of project div

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ import ytClone from "../assets/ytClone.png"
 const Projects = () => {
   return (
    <div id="projects" className='w-[95%] max-w-7xl flex flex-wrap gap-8 mx-auto my-10 justify-center transition-all duration-300'>
-    <a href='https://github.com/AadityaBajgain/MoneyMap' className='w-full sm:w-[45%] lg:w-[30%] transition-transform duration-300 hover:scale-105'>
+    <a href='https://github.com/AadityaBajgain/MoneyMap' className='w-[80vw] sm:w-[45%] lg:w-[30%] transition-transform duration-300 hover:scale-105'>
       <div className=' h-[40vh] border-2 border-slate-200 rounded-lg relative group overflow-hidden'>
         <img className="w-full h-full object-cover blur-[2px] group-hover:blur-[4px] transition-all" src={moneymap} alt="MoneyMap" />
         <div className="absolute inset-0 flex items-center justify-center">
@@ -12,7 +12,7 @@ const Projects = () => {
         </div>
       </div>
     </a>
-    <a href='https://github.com/AadityaBajgain/YT-clone' className='w-full sm:w-[45%] lg:w-[30%] transition-transform duration-300 hover:scale-105'>
+    <a href='https://github.com/AadityaBajgain/YT-clone' className='w-[80vw] sm:w-[45%] lg:w-[30%] transition-transform duration-300 hover:scale-105'>
       <div className='h-[40vh] border-2 border-slate-200  rounded-lg relative group'>
       <img className="w-full h-full object-cover blur-[2px] group-hover:blur-[4px] transition-all" src={ytClone} alt="Youtube Clone" />
         <div className="absolute inset-0 flex items-center justify-center">
@@ -20,7 +20,7 @@ const Projects = () => {
         </div>
       </div>
     </a>
-    <a href='https://github.com/BarsatKhadka/PrepAI' className='w-full sm:w-[45%] lg:w-[30%] transition-transform duration-300 hover:scale-105'>
+    <a href='https://github.com/BarsatKhadka/PrepAI' className='w-[80vw] sm:w-[45%] lg:w-[30%] transition-transform duration-300 hover:scale-105'>
       <div className='h-[40vh] border-2 border-slate-200 rounded-lg relative group'>
         <div className="absolute inset-0 flex items-center justify-center">
           <h3 className="text-gray-600 text-2xl font-bold">PrepAI</h3>
