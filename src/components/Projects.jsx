bd6cfc16e10b333b3897c5b9ae2272ee77538b1c
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-03-06 15:11:01 -0800

    add PrepAI project image to Projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import moneymap from "../assets/moneymap.png"
 import ytClone from "../assets/ytClone.png"
+import prepai from "../assets/prepai.png"
 const Projects = () => {
   return (
    <div id="projects" className='w-[95%] max-w-7xl flex flex-wrap gap-8 mx-auto my-10 justify-center transition-all duration-300'>
@@ -23,6 +24,7 @@ const Projects = () => {
     <a href='https://github.com/BarsatKhadka/PrepAI' className='w-[80vw] sm:w-[45%] lg:w-[30%] transition-transform duration-300 hover:scale-102'>
       <div className='h-[30vh] sm:h-[40vh] border-2 border-slate-200 rounded-lg relative group'>
         <div className="absolute inset-0 flex items-center justify-center">
+        <img className="w-full h-full object-cover blur-[2px] group-hover:blur-[4px] transition-all" src={prepai} alt="Youtube Clone" />
           <h3 className="text-gray-600 text-2xl font-bold">PrepAI</h3>
         </div>
       </div>
